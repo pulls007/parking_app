1191c727d6414726ec71ebd6dcf84940f4bc2a7b
Pulkit Dahuja <pulkit.d@chaipoint.com>
2022-03-13 20:01:40 +0530

    initial commit

---
+++ parking_lot_first/src/main/java/com/designpattern/parking_lot_first/ParkingLotFirstApplication.java
@@ -0,0 +1,14 @@
+package com.designpattern.parking_lot_first;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingLotFirstApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParkingLotFirstApplication.class, args);
+        System.out.println("First commit ");
+    }
+
+}
---
+++ parking_lot_first/src/test/java/com/designpattern/parking_lot_first/ParkingLotFirstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.designpattern.parking_lot_first;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingLotFirstApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
